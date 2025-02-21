cfa8005f2e457728473900be94757808e634c212
GreenestGoat <fionnanloughlin@gmail.com>
2025-02-21 23:14:35 +0000

    Added Vimeo to +page.svelte

--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-    import { YouTube, Vimeo } from '$lib/index.js';
+  import { YouTube, Vimeo } from '$lib/index.js';
 </script>
 
 <YouTube 
   id="KRVnaN29GvM" 
   posterquality="max"
   title="Rock the Boat"
+/>
+
+<Vimeo 
+  id="32001208" 
+  posterquality="max"
+  title="Earth"
 />
