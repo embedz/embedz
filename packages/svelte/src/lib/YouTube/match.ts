74150d4c0cbd03a074160ad5eeea5a824e1fc596
GreenestGoat <fionnanloughlin@gmail.com>
2025-02-26 23:05:21 +0000

    Added music.youtube.com to regex

--- packages/svelte/src/lib/YouTube/match.ts
+++ packages/svelte/src/lib/YouTube/match.ts
@@ -1,7 +1,7 @@
 function match(input: string): string | null {
     if (!input) return null;
 
-    let r = /(?:youtu\.be\/|youtube\.com\/(?:shorts\/|embed\/|.*[?&]v=))([A-Za-z0-9_-]{11})|^([A-Za-z0-9_-]{11})$/;
+    let r = /(?:youtu\.be\/|(?:music\.)?youtube\.com\/(?:shorts\/|embed\/|.*[?&]v=))([A-Za-z0-9_-]{11})|^([A-Za-z0-9_-]{11})$/;
     let m = input.match(r);
 
     return m ? m[1] || m[2] : null;
